09d518cead7bd912beaa57b14731a072ed4bb201
jian <jianlee.dev@gmail.com>
2021-03-25 18:30:01 +0900

    bin폴더 속  파일도 수정

---
+++ bin/calender/service/CommonService.java
@@ -0,0 +1,23 @@
+package calender.service;
+
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+public class CommonService implements ICommonService {
+
+	@Override
+	public void close(Parent root) {
+		((Stage)root.getScene().getWindow()).close();
+
+	}
+
+	@Override
+	public void alert(AlertType alertType, String message) {
+		new Alert(alertType,message).show();
+		
+		
+	}
+
+}
---
+++ bin/calender/service/ILoginService.java
@@ -0,0 +1,5 @@
+package calender.service;
+
+public interface ILoginService {
+
+}
