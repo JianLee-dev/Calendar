2571c52c9bfa6c74f8fe21d18828c50b00a17086
jeongwoo <dahlia11444@gmail.com>
2021-04-03 20:41:31 +0900

    캘린더 디비 저장기능 구현

--- src/calendar/add/CAddController.java
+++ src/calendar/add/CAddController.java
@@ -8,34 +8,49 @@ import java.util.ResourceBundle;
 import calendar.calendar.info.CalendarInfoController;
 import calendar.calendar.info.CalendarInfoMain;
 import calendar.common.CommonService;
+import calendar.datadase.CalendarVO;
+import calendar.datadase.DatabaseService;
+import calendar.datadase.IDatabaseService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class CAddController implements Initializable {
 	Parent root;
-
+	CAddSurvice cs;
+	IDatabaseService ds;
+	
 	public void setRoot(Parent root) {
 		this.root = root;
-		
+		cs.setRoot(root);
+		cs.setComboBox();
 	}
+
+
+	
 	public void close() {
 		CommonService.close(root);
 	}
 
 	public void save() {
+		CalendarVO vo = cs.save();
+		 ds.saveCalendar(vo);
+		
+	
 		
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+		cs = new CAddSurvice();
+		ds = new DatabaseService();
 	}
 
 
