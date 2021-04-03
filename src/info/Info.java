a282b4e606fab401a4ed6842793248fb764ebbd9
spammy <spammy7147@gmail.com>
2021-04-03 21:44:46 +0900

    caledar 쪽 static 흐름 정리 파일 업데이트

---
+++ src/info/Info.java
@@ -0,0 +1,26 @@
+package info;
+public class Info {
+
+	/*
+	 캘린더 내부의 TableView 의 내용 DB에서 받아서 세팅하는 방법
+	 CalendarInfoMain.calendarInfoController.setTable();
+	
+
+	 CService 클래스에서 mouseOnClicked 메서드에서 Date 선언 현재 클릭한 날짜의 정보를 저장하려면 여기서 설정 또는 많은 클래스에서 이 정보를 필요시 
+	 클래스 static 변수로 선언해두고 클릭할떄마다 변경해주는걸로 변경 할 수 있음
+	 
+	 mouseOnClicked 메서드 내부에서 이렇게 클릭한 날짜를 넘겨주고 있음! 위의 내용으로 변경가능!
+	 CalendarInfoService.date= fDate; //CalendarInfoService 에 날짜정보 저장
+	 
+	 
+	 
+	 
+	 
+	 */
+	
+	
+	
+	
+	
+	
+}
