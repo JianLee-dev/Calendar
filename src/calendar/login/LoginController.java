13947ab6f62cdb763b6a8d4f682b0f0489979183
jian <jianlee.dev@gmail.com>
2021-04-05 15:15:05 +0900

    edit rememberId Function

--- src/calendar/login/LoginController.java
+++ src/calendar/login/LoginController.java
@@ -1,5 +1,14 @@
 package calendar.login;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -34,6 +43,7 @@ public class LoginController implements Initializable{
 	public static UserVO user;
 	private ILoginService ls;
 	
+	private String savedUser;
 	
 
 	public void setRoot(Parent root) {
@@ -44,6 +54,13 @@ public class LoginController implements Initializable{
 	
 	
 	public void confirm() {
+		TextField loginId = (TextField)root.lookup("#userId");
+		if(ckId.isSelected()==true) {
+			saveUserId(loginId.getText());
+		}else {
+			saveUserId(" ");
+		}
+		
 		if(ls.loginCheck(root)) { 				//비밀번호 확인
 			ls.setLogin(root);					//로그인 사용자정보 저장 
 			new MainForm(); 					// MainForm 오픈
@@ -51,8 +68,7 @@ public class LoginController implements Initializable{
 			CommonService.close(root); 			//로그인창 닫기
 		}else { //아이디또는 비밀번호 틀릴시 진입 	
 			CommonService.alert(AlertType.WARNING, "없는 사용자 이거나 잘못된 비밀번호 입니다."); 
-			//((PasswordField)root.lookup("#userPw")).clear(); 								//비밀번호 오류시 비밀번호 삭제
-			clear();
+			((PasswordField)root.lookup("#userPw")).clear(); 								//비밀번호 오류시 비밀번호 삭제
 		}
 		
 		
@@ -64,15 +80,43 @@ public class LoginController implements Initializable{
 		new MemberMain(); 						//회원가입 누를시 회원가입창 오픈
 	}
 	
-	
-	private void clear() {
-		if(ckId.isSelected()==true) {
-			userPw.clear();
-			userPw.requestFocus();
-		}else {
-			userId.clear(); userPw.clear();
-			userId.requestFocus();
+	private void saveUserId(String userId) {
+		try {
+			//I:\\오후_취업반\\test\\savedId.txt
+			File path = new File("I:\\오후_취업반\\test\\savedId.txt");
+			FileOutputStream fos;
+			fos = new FileOutputStream(path);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			DataOutputStream dos = new DataOutputStream(bos);
+			
+			dos.writeUTF(userId);
+			
+			dos.close(); bos.close(); fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
+	
+	private void setLoginText() {
+		try {
+			File path = new File("I:\\오후_취업반\\test\\savedId.txt");
+			FileInputStream fis = new FileInputStream(path);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			DataInputStream dis = new DataInputStream(bis);
+		
+			savedUser = dis.readUTF();
+			if(!savedUser.equals(" ")) {
+				userId.setText(savedUser);
+				userPw.requestFocus();
+			}else {
+				userId.requestFocus();
+			}
+			
+			dis.close(); bis.close(); fis.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	
 	}
 	
 	public void findId() {
@@ -88,6 +132,6 @@ public class LoginController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		ls = new LoginService();
-		
+		setLoginText();
 	}
 }
